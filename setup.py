1519c130dfe46f170b7cdbcf1974f91071949fd0
SVAKSHA <svaksha@gmail.com>
2013-09-14 02:45:22 +0530

    recursively include the top-level data directory

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read():
     with open('README.rst') as f:
         return f.readfile()
 
-setup(name='chaya',
+setup(name = 'chaya',
       version = chaya.__version__,
       author = 'SVAKSHA',
       author_email = 'svaksha@gmail.com',
@@ -14,21 +14,25 @@ setup(name='chaya',
       description ='Learn computer vision, edge detection',
       long_description = open('README.rst').read()
       packages = ['chaya'],
-      package_data = {'chaya': ['LICENSE.rst']},
+      package_data = {'chaya': ['LICENSE.rst',
+                                'AUTHORS.rst',
+                                'MANIFEST.in',
+                                'README.rst',
+                                ]},
       include_package_data = True,
       install_requires = ['Cython==0.19.1',
                           'Pillow==2.1.0',
-                          'distribute==0.6.34',
                           'numpy==1.7.1',
                           ],
-      classifiers=[
-        'Development Status :: 13.08 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: AGPLv3 License',
-        'Programming Language :: Python :: 3.3',
-        'Topic :: Graphics :: Computer Vision',
-        ],
-      packages=['chaya', 'chaya.tests'], #TODO
+      classifiers = ['Development Status :: 13.08 - Alpha',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: AGPLv3 License',
+                     'Programming Language :: Python :: 3.3',
+                     'Topic :: Graphics :: Computer Vision',
+                     ],
+      packages = ['chaya',
+                  'chaya.tests',
+                  ],   #TODO
       )
 
 
